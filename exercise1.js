3d04d12d0467aca5dd3c3db79558db827166aaba
unknown <ahmadsafrizal01@gmail.com>
2023-08-27 10:15:23 +0700

    ecercise 1 26-8-2023

---
+++ exercise1.js
@@ -0,0 +1,11 @@
+for (let fizzBuzz = 1; fizzBuzz <= 100; fizzBuzz++) {
+  if (fizzBuzz % 3 === 0 && fizzBuzz % 5 !== 0) {
+    console.log ("Fizz")
+  } else if (fizzBuzz % 5 === 0 && fizzBuzz % 3 !== 0) {
+    console.log ("Buzz")
+  } else if (fizzBuzz % 3 === 0 && fizzBuzz % 5 === 0) {
+    console.log ("FizzBuzz")
+  } else {
+    console.log (fizzBuzz)
+  }
+}
